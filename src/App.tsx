cb7955d5f8694a9185ce6448c479a6f75e9cc290
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-05-24 21:28:27 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Demo from './containers/Demo';
+
+function App() {
+  return (
+    <div className="app">
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Demo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Demo.module.scss';
+
+const Demo = () => {
+  return <div className={classes.textColor}>Hello</div>;
+};
+
+export default Demo;
